520775a5cae4791f667d05bb32425991e64d6084
Andy Mabbutt <andymabb@users.noreply.github.com>
2024-01-23 19:05:21 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+$page = basename($_SERVER["PHP_SELF"]);
+?>
 <!doctype html>
 <html class="no-js" lang="en-GB">
 
@@ -13,39 +16,32 @@
   <meta property="og:url" content="">
   <meta property="og:image" content="">
   
-  <?php include("inc/favicons.php); ?>
+  <?php include("inc/favicons.php"); ?>
  
 </head>
 
 <body>
 <header class="header">
-<?php include("inc/header.php); ?>
-<?php include("inc/mainnav.php); ?>
+<?php include("inc/header.php"); ?>
+<?php include("inc/mainnav.php"); ?>
 </header>
  <main class="main">
  <p>Hello world!</p>
- primary
- secondary
- tertiary
- light
- accent
- accent--alt
- --accent--ui
- 
- :root {
-    /* Color styles */
-    --primary: hsl(265, 38%, 13%);
-    --secondary: hsl(283, 6%, 45%);
-    --tertiary: hsl(257, 15%, 91%);
-    --light: hsl(270, 100%, 99%);
-    --accent: hsl(278, 100%, 92%);
-    --accent--alt: hsl(279, 100%, 97%);
-    --accent--ui: hsl(284, 55%, 66%);
-  }
+ <div style="display:grid; grid-template-columns: repeat(
+      auto-fit, 100px); grid-template-rows: 100px;">
+   <div style="background-color: var(--primary);">--primary: hsl(265, 38%, 13%);</div>
+   <div style="background-color: var(--secondary);">--secondary: hsl(283, 6%, 45%);</div>
+   <div style="background-color: var(--tertiary);">--tertiary: hsl(257, 15%, 91%);</div>
+   <div style="background-color: var(--light);">--light: hsl(270, 100%, 99%);</div>
+   <div style="background-color: var(--accent);">--accent: hsl(278, 100%, 92%);</div>
+   <div style="background-color: var(--accent--alt);">--accent--alt: hsl(279, 100%, 97%);</div>
+   <div style="background-color: var(--accent--ui);">--accent--ui</div>
+ </div>
+
  </main>
 
 <footer class="footer">
-<?php include("inc/footer.php); ?>   
+<?php include("inc/footer.php"); ?>   
 </footer>
  
 
