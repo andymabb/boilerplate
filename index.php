ad06e940b24ec21f3434a95000e5611612dcb14d
Andy Mabbutt <andymabb@users.noreply.github.com>
2024-01-23 09:55:52 +0000

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html class="no-js" lang="en-GB">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+  <link rel="stylesheet" href="css/style.css">
+  <meta name="description" content="">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+  
+  <?php include("inc/favicons.php); ?>
+ 
+</head>
+
+<body>
+
+ <p>Hello world!</p>
+
+
+</body>
+
+</html>
