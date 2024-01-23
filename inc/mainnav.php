77a49265310160ac09e55d7a493753cbab80763e
Andy Mabbutt <andymabb@users.noreply.github.com>
2024-01-23 10:02:22 +0000

    Create mainnav.php

---
+++ inc/mainnav.php
@@ -0,0 +1,4 @@
+<ul class="mainnav nav">
+    <li><a aria-current="<?php if($page=='index.php'){echo 'page';}?>" href="index.php">Home</a></li>
+    <li><a aria-current="<?php if($page=='about.php'){echo 'page';}?>" href="about.php">About</a></li>
+</ul>
